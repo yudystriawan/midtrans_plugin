260581adae110b03972b67b4fe6fbbf863da6b44
yudystriawan <yudistriawan@gmail.com>
2024-02-24 18:30:04 +0800

    migrate to ObjC

---
+++ ios/Classes/MidtransPlugin.m
@@ -0,0 +1,20 @@
+#import "MidtransPlugin.h"
+
+@implementation MidtransPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"midtrans_plugin"
+            binaryMessenger:[registrar messenger]];
+  MidtransPlugin* instance = [[MidtransPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
