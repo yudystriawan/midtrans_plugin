c25e3c72e1519854a354511c0cc7dc6e1d79f693
yudystriawan <yudistriawan@gmail.com>
2024-11-28 19:01:03 +0800

    feat: export MidtransTransactionResult model in models.dart

--- packages/midtrans_platform_interface/lib/models/models.dart
+++ packages/midtrans_platform_interface/lib/models/models.dart
@@ -3,3 +3,4 @@ export 'midtrans_config.dart';
 export 'midtrans_failure.dart';
 export 'midtrans_item_details.dart';
 export 'midtrans_transaction_details.dart';
+export 'midtrans_transaction_result.dart';
