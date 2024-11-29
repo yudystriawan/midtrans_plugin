3a2e3f592e59f5938c6f81834762c2fcf6f2c860
yudystriawan <yudistriawan@gmail.com>
2024-11-29 11:00:33 +0800

    refactor: remove unused ItemDetails, TransactionDetails, and TransactionResult models

--- example/ios/.symlinks/plugins/midtrans_ios/ios/Classes/models/TransactionDetails.swift
+++
@@ -1,9 +0,0 @@
-struct TransactionDetails {
-  let orderId: String
-  let grossAmount: NSNumber
-  
-  init(dictionary: [String: Any]) {
-    orderId = dictionary["orderId"] as! String
-    grossAmount = dictionary["grossAmount"] as! NSNumber
-  }
-}
